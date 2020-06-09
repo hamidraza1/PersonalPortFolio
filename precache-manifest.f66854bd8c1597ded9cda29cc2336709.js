1d01f659cb843623f80c4d4a42b14f4f613d971c
hamidraza1 <hrazaa306@gmail.com>
2020-06-09 20:29:16 +0200

    Updates

---
+++ precache-manifest.f66854bd8c1597ded9cda29cc2336709.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262b75b18ec86b0f58b3f9993597e5c9",
+    "url": "/PersonalPortFolio/index.html"
+  },
+  {
+    "revision": "44c28fbbe66a1d7a9c3e",
+    "url": "/PersonalPortFolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e8bce29318ab5b03661a",
+    "url": "/PersonalPortFolio/static/css/main.2e5c531d.chunk.css"
+  },
+  {
+    "revision": "44c28fbbe66a1d7a9c3e",
+    "url": "/PersonalPortFolio/static/js/2.2ca92f48.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/PersonalPortFolio/static/js/2.2ca92f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8bce29318ab5b03661a",
+    "url": "/PersonalPortFolio/static/js/main.3fc5b80d.chunk.js"
+  },
+  {
+    "revision": "c46a56d1c24451459d9d",
+    "url": "/PersonalPortFolio/static/js/runtime-main.fac8d704.js"
+  },
+  {
+    "revision": "958cdb178c8946d86af1dee22cd59ef3",
+    "url": "/PersonalPortFolio/static/media/pppp.958cdb17.jpg"
+  },
+  {
+    "revision": "863a8360a6f42ad12a89ad2f10a00670",
+    "url": "/PersonalPortFolio/static/media/pppp1.863a8360.png"
+  }
+]);
